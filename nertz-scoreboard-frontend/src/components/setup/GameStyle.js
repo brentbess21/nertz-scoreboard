f57fe1b2ea9add3f0a74b5c9659419cd5098333a
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-15 11:34:43 -0700

    Added Winning Score and Number of Rounds links to GameStyle component

--- nertz-scoreboard-frontend/src/components/setup/GameStyle.js
+++ nertz-scoreboard-frontend/src/components/setup/GameStyle.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from './../app/Header';
 
 const GameStyle = () => {
     return(
         <div>
-            <h1>Game Style?</h1>
+            <Header />
+            <h1>How Would You Like to Play?</h1>
+            <Link to='/' className='link-button'>Winning Score</Link>
+            <Link to='/' className='link-button'>Number of Rounds</Link>
         </div>
     )
 }
