57f9a6d9b36d3c865c2b931016beaee6f29edadf
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-15 11:25:16 -0700

    Imported and routed GameStyle component in App.js

--- nertz-scoreboard-frontend/src/components/app/App.js
+++ nertz-scoreboard-frontend/src/components/app/App.js
@@ -4,11 +4,13 @@ import { Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Rules from './Rules';
+import GameStyle from '../setup/GameStyle';
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path='/gamestyle' element={<GameStyle />}/>
         <Route path='/rules' element={<Rules />}/>
         <Route path='/about' element={<About />}/>
         <Route exact path ='/' element={<Home />}/>
--- nertz-scoreboard-frontend/src/components/app/Home.js
+++ nertz-scoreboard-frontend/src/components/app/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <Header />
             <h1>Welcom to the Nerts Scoreboard!</h1>
             <div>
-                <Link to='/' className='link-button'>New Game</Link>
+                <Link to='/gamestyle' className='link-button'>New Game</Link>
                 <Link to='/' className='link-button'>Resume Game</Link>
             </div>
             
