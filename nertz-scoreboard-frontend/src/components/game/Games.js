c62f28c5bc26df422be49d2ae6a9f7fb960a4cdf
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-23 14:16:28 -0700

    Added conditional in Game component so if no games exist yet, it asks you to create a new game

--- nertz-scoreboard-frontend/src/components/game/Games.js
+++ nertz-scoreboard-frontend/src/components/game/Games.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Header from '../app/Header';
 
 const Games = (props) => {
 
-    const clickHandler = () => {
-        console.log(props.games)
+    if(props.games.length < 1) {
+        return(
+            <div>
+                <Header />
+                <h1>There are no saved games!</h1>
+                <p>Start A New Game To Get Started!</p>
+                <Link to='/gamestyle' className='link-button'>New Game</Link>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Header />
+                <h1>Pick a Game!</h1>
+                {props.games.map(game => {
+                    return <Link className='link-button' to={`/games_list/${game.gameId}`} key={game.gameId}>{game.gameId}</Link>
+                
+                })}
+            </div>
+        )
     }
-    return (
-        <div>
-            <Header />
-            <h1>Pick a Game!</h1>
-            {props.games.map(game => {
-                return <Link className='link-button' to={`/games_list/${game.gameId}`} key={game.gameId}>{game.gameId}</Link>
-            
-            })}
-            <button onClick={clickHandler}>Click me for them props!!</button>
-            <Outlet />
-        </div>
-    )
+    
 }
 
 const mapStateToProps = (state) => {
