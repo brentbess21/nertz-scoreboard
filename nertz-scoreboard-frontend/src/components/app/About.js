0d654d64ce694ffda4e1e8dae19868ee0e805a03
Brent Besselievre <brent@Brents-MacBook-Air.local>
2022-01-04 18:18:51 -0700

    Quick stlying of About component

--- nertz-scoreboard-frontend/src/components/app/About.js
+++ nertz-scoreboard-frontend/src/components/app/About.js
@@ -4,11 +4,13 @@ import Header from './Header';
 
 const About = () => {
     return(
-        <div className='container'>
+        <div className='home'>
             <Header />
-            <h1>About The Nerts Scoreboard</h1>
-
-            <p>This website was built so you can keep track of the score of each player playing a game of Nerts</p>
+            <div className='grid-container flow'>
+                <h1 className='txt-light title uppercase'>About the <span className='title-bottom display-block txt-dark fs-800'>Nerts Score Board </span></h1>
+                <p>This website was built so you can keep track of the score of each player playing a game of Nerts</p>
+            </div>
+            
         </div>
     )
 }
